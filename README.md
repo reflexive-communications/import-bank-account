62ec05fc6f5548bb6abd43105501e4e12d69c24a
Sandor Semsey <sandor@es-progress.hu>
2022-03-08 12:06:37 +0100

    initial

---
+++ README.md
@@ -0,0 +1,27 @@
+# import-bank-account
+
+This extension provides a custom importer.
+
+Constribution import where the contact mapping is based on a contact custom field. If you want to execute this importer,
+navigate to the `Contributions > Import Contributions (Bank Account)` menu. The data mapping screen provides the custom
+fields as options for contact mapping instead of the legacy contact mapping options.
+
+For the mapping to work, you need to set the bank account numbers for each contact beforehand. After this, the importer
+can find the contact based on the account number and add the contribution.
+
+The extension is licensed under [AGPL-3.0](LICENSE.txt).
+
+## Requirements
+
+* PHP v7.4+
+* CiviCRM v5.43
+
+## Installation (CLI, Git)
+
+Sysadmins and developers may clone the [Git](https://en.wikipedia.org/wiki/Git) repo for this extension and install it
+with the command-line tool [cv](https://github.com/civicrm/cv).
+
+```bash
+git clone https://github.com/reflexive-communications/import-bank-account.git
+cv ext:enable import-bank-account
+```
---
+++ import_bank_account.php
@@ -0,0 +1,184 @@
+<?php
+
+require_once 'import_bank_account.civix.php';
+
+// phpcs:disable
+use CRM_ImportBankAccount_ExtensionUtil as E;
+
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function import_bank_account_civicrm_config(&$config)
+{
+    _import_bank_account_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
+ */
+function import_bank_account_civicrm_xmlMenu(&$files)
+{
+    _import_bank_account_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function import_bank_account_civicrm_install()
+{
+    _import_bank_account_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function import_bank_account_civicrm_postInstall()
+{
+    _import_bank_account_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function import_bank_account_civicrm_uninstall()
+{
+    _import_bank_account_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function import_bank_account_civicrm_enable()
+{
+    _import_bank_account_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function import_bank_account_civicrm_disable()
+{
+    _import_bank_account_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function import_bank_account_civicrm_upgrade($op, CRM_Queue_Queue $queue = null)
+{
+    return _import_bank_account_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+ */
+function import_bank_account_civicrm_managed(&$entities)
+{
+    _import_bank_account_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Add CiviCase types provided by this extension.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
+ */
+function import_bank_account_civicrm_caseTypes(&$caseTypes)
+{
+    _import_bank_account_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Add Angular modules provided by this extension.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
+ */
+function import_bank_account_civicrm_angularModules(&$angularModules)
+{
+    // Auto-add module files from ./ang/*.ang.php
+    _import_bank_account_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
+ */
+function import_bank_account_civicrm_alterSettingsFolders(&$metaDataFolders = null)
+{
+    _import_bank_account_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function import_bank_account_civicrm_entityTypes(&$entityTypes)
+{
+    _import_bank_account_civix_civicrm_entityTypes($entityTypes);
+}
+
+/**
+ * Implements hook_civicrm_themes().
+ */
+function import_bank_account_civicrm_themes(&$themes)
+{
+    _import_bank_account_civix_civicrm_themes($themes);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function import_bank_account_civicrm_preProcess($formName, &$form) {
+//
+//}
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ */
+//function import_bank_account_civicrm_navigationMenu(&$menu) {
+//  _import_bank_account_civix_insert_navigation_menu($menu, 'Mailings', [
+//    'label' => E::ts('New subliminal message'),
+//    'name' => 'mailing_subliminal_message',
+//    'url' => 'civicrm/mailing/subliminal',
+//    'permission' => 'access CiviMail',
+//    'operator' => 'OR',
+//    'separator' => 0,
+//  ]);
+//  _import_bank_account_civix_navigationMenu($menu);
+//}
